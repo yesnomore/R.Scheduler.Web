dda575318f894c0acea59aa9804d75060dae8f66
jpachansky <jpachansky@gmail.com>
2014-09-15 14:47:16 +0100

    Experiment with Cherrrypy

---
+++ r_scheduler_web/server.py
@@ -0,0 +1,32 @@
+import cherrypy
+import os
+
+# if you have a config, import it here
+# from myproj import config
+
+from controllers import Root
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+
+def get_app_config():
+    return {
+        '/static': {
+            'tools.staticdir.on': True,
+            'tools.staticdir.dir': os.path.join(HERE, 'static'),
+        }
+    }
+
+def get_app(config=None):
+    #config = config or get_config()
+    cherrypy.tree.mount(Root(), '/')
+    return cherrypy.tree
+
+
+def start():
+    get_app()
+    cherrypy.engine.signals.subscribe()
+    cherrypy.engine.start()
+    cherrypy.engine.block()
+
+if __name__ == '__main__':
+    start()
